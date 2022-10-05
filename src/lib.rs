7594d4da716083e50348b1251c06de8e89189b5e
Juliette Cordor <grassojames5@gmail.com>
2022-10-05 13:34:49 +1100

    fid

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 use std::future::Future;
 
+pub type QueueFn = dyn Fn() + Send + Sync + 'static;
+
 pub struct QueueItem {
-    func: Box<dyn Fn() + Send + Sync + 'static>,
+    func: Box<QueueFn>,
     promise: Option<Box<dyn Future<Output = ()>>>,
 }
 
 impl QueueItem {
-    pub fn new(func: impl Fn() + Send + Sync + 'static) -> Self {
+    pub fn new(func: &QueueFn) -> Self {
         Self {
             func: Box::new(func),
             promise: None,
@@ -30,5 +32,10 @@ pub struct Queue {
 impl Queue {
     pub const fn new() -> Self {
         Self { queue: Vec::new() }
+    }
+
+    pub fn add(&mut self, func: &QueueFn) {
+        let item = QueueItem::new(func);
+        self.queue.push(item);
     }
 }
