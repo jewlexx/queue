47bc8436a9d653209d7538cf0f642df92a3687c0
Juliette Cordor <grassojames5@gmail.com>
2022-10-05 13:18:23 +1100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::future::Future;
 
 pub struct QueueItem {
     func: Box<dyn Fn() + Send + Sync + 'static>,
-    promise: Box<dyn Future<Output = ()>>,
+    promise: Option<Box<dyn Future<Output = ()>>>,
 }
 
 pub struct Queue {
