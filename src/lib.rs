c0b18f198ae3f3ccfa2d7ee781c12e22d9596e05
Juliette Cordor <grassojames5@gmail.com>
2022-10-06 09:52:42 +1100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -60,11 +60,11 @@ impl<F: QueueFn> Queue<F> {
 
             queue.retain(|item| !item.is_finished());
 
-            // for mut item in queue.iter() {
-            //     let func = Box::new(&item.func);
+            for mut item in queue {
+                let func = Box::new(&item.func);
 
-            //     let thread = std::thread::spawn(func);
-            // }
+                let thread = std::thread::spawn(func);
+            }
         }
     }
 }
